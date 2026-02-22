31e767f92d92aba725d6f70aa7c4edb497f5a3d4
Md. Hanan Sajid <hnn@mbpm1m.local>
2026-02-22 16:49:51 +0530

    basic page API endpoints

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,26 @@ app.get("/", (req, res) => {
     res.json({message: "Welcome to the VIT Student Reminder App API!"});
 });
 
+app.get("/academics", (req, res) => {
+    res.json({message: "This is the Academics page API endpoint."});
+});
+
+app.get("/events", (req, res) => {
+    res.json({message: "This is the Events page API endpoint."});
+});
+
+app.get("/clubs", (req, res) => {
+    res.json({message: "This is the Clubs page API endpoint."});
+});
+
+app.get("/hostel", (req, res) => {
+    res.json({message: "This is the Hostel page API endpoint."});
+});
+
+app.get("/profile", (req, res) => {
+    res.json({message: "This is the Profile page API endpoint."});
+});
+
 app.listen(5001, () =>{
     console.log("Server started on PORT: 5001");
 })
